cbfbb21fc9a70d9f27f03075d7b5fede11ba817c
IhorShcherbak <157756063+IhorShcherbak@users.noreply.github.com>
2025-03-08 19:49:01 +0000

    changed elements

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,11 @@
 function getElementWidth(content, padding, border) {
     
    
-   const Width = Number.parseInt (content) + Number.parseInt (padding) * 2 + Number.parseFloat (border) * 2;
-    return Width;
-   
-}
+    const chapterWidth = Number.parseFloat(content) + Number.parseFloat(padding) * 2 + Number.parseFloat(border) * 2;
+    return chapterWidth;
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+}
+    
+    console.log(getElementWidth("50px", "8px", "4px")); // 74
+    console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+    console.log(getElementWidth("200px", "0px", "0px")); // 200
